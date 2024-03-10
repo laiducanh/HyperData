98f9481a72a37a18e3c23cd6c31579a01dc430bd
LDA <laiducanh20.3@gmail.com>
2024-03-09 20:06:58 -0600

    clean up settings.py

--- HyperData/config/settings.py
+++ HyperData/config/settings.py
@@ -1,95 +1,80 @@
-import string, itertools, matplotlib, os, logging
+import string, itertools, matplotlib, os, logging, qfluentwidgets
 from PyQt6.QtCore import Qt, QStandardPaths, QDir, QSettings
-from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout
+from ui.base_widgets.button import ComboBox
 
 DEBUG = True
 
 # list_name is replaced by column_labels
 list_name =list(string.ascii_lowercase)
-for j in [''.join(i) for i in itertools.product(string.ascii_lowercase, repeat = 2)]:
-    list_name.append(j) # can handle maximum of 702 columns
-
-cmap_map = {
-        'Perceptually Uniform Sequential':['viridis', 'plasma', 'inferno', 'magma', 'cividis'],
-        'Sequential':['Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
-                      'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
-                      'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn'],
-        'Sequential (2)':['binary', 'gist_yarg', 'gist_gray', 'gray', 'bone',
-                      'pink', 'spring', 'summer', 'autumn', 'winter', 'cool',
-                      'Wistia', 'hot', 'afmhot', 'gist_heat', 'copper'],
-        'Diverging':['PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu',
-                      'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic'],
-        'Cyclic':['twilight', 'twilight_shifted', 'hsv'],
-        'Qualitative': ['Pastel1', 'Pastel2', 'Paired', 'Accent', 'Dark2',
-                      'Set1', 'Set2', 'Set3', 'tab10', 'tab20', 'tab20b',
-                      'tab20c'],
-        'Miscellaneous':['flag', 'prism', 'ocean', 'gist_earth', 'terrain',
-                      'gist_stern', 'gnuplot', 'gnuplot2', 'CMRmap',
-                      'cubehelix', 'brg', 'gist_rainbow', 'rainbow', 'jet',
-                      'turbo', 'nipy_spectral', 'gist_ncar'],}
+for j in [''.join(i) for i in itertools.product(string.ascii_lowercase, repeat = 2)]: list_name.append(j) # can handle maximum of 702 columns
 
 from matplotlib import font_manager
 font_lib = font_manager.get_font_names()
 font_lib.append('sans-serif')
 
-linestyle_lib = {'-': 'Solid',
-              '--': 'Dashed',
-              '-.': 'DashDot',
-              ':': 'Dotted',
-              'None': 'None',
-              }
-
-marker_lib = {'.': 'point', ',': 'pixel', 'o': 'circle', 'v': 'triangle down', '^': 'triangle up', '<': 'triangle left',
-              '>': 'triangle right', '1': 'tri down', '2': 'tri up', '3': 'tri left', '4': 'tri right', '8': 'octagon', 
-              's': 'square', 'p': 'pentagon', '*': 'star', 'h': 'hexagon1', 'H': 'hexagon2', '+': 'plus', 'x': 'x', 
-              'D': 'diamond', 'd': 'thin_diamond', '|': 'vline', '_': 'hline', 'P': 'plus filled', 'X': 'x filled', 
-              0: 'tickleft', 1: 'tickright', 2: 'tickup', 3: 'tickdown', 4: 'caretleft', 5: 'caretright', 6: 'caretup', 
-              7: 'caretdown', 8: 'caretleftbase', 9: 'caretrightbase', 10: 'caretupbase', 11: 'caretdownbase', 
-              'None': 'none'}
-
-hatch_lib = ['/', '\\', '|', '-', '+', 'x', 'o', 'O', '.', '*']
-
-model_dict = {'model 1':{'algorithm':{'algorithm':'none',},'X train':'',"Y train":'','X validate':'','Y validate':'','result':'',
-                         'test option':'percentage split','k-fold':5,'train repeat':1,'percentage split':0.2,}}
-
+marker_lib = matplotlib.lines.Line2D.markers
 
 matplotlib.rcParams['font.family'] = 'DejaVu Sans'
 
-#seaborn.set_palette(palette='bright',n_colors=20)    
 color_lib = matplotlib.rcParams["axes.prop_cycle"].by_key()["color"]
 
-if matplotlib.rcParams['legend.fancybox']:
-        style = 'round'
-else:
-        style = 'square'
+linestyle_lib = {'-': 'Solid','--': 'Dashed','-.': 'DashDot',':': 'Dotted','None': 'None',}
+
+hatch_lib = ['/', '\\', '|', '-', '+', 'x', 'o', 'O', '.', '*']
 
 ### Initialize setting and logging profiles
 dataPath = QStandardPaths.writableLocation(QStandardPaths.StandardLocation.AppDataLocation)
 dataPathDir = QDir(dataPath)
 if not dataPathDir.exists():
-        # create the directory (including parent directories if they don't exist);
-        # that the argument of mkpath is relative to the QDir's object path, so
-        # using '.' means that it will create the actual dataPath
-        dataPathDir.mkpath('.')  
+    # create the directory (including parent directories if they don't exist);
+    # that the argument of mkpath is relative to the QDir's object path, so
+    # using '.' means that it will create the actual dataPath
+    dataPathDir.mkpath('.')  
 
 appName = 'HyperData'
+configFile = os.path.join(dataPathDir.absolutePath(),appName,"config.ini")
+if DEBUG: configFile = "config.ini"
+config = QSettings(configFile, QSettings.Format.IniFormat)
+
 logFile = os.path.join(dataPathDir.absolutePath(),appName,"log.txt")
 if DEBUG: logFile = "DEBUG.txt"
 logging.getLogger('matplotlib.font_manager').disabled = True
 # Create and configure logger
-logging.basicConfig(filename=logFile,
-                    format='%(asctime)s %(message)s',
-                    filemode='w')
+logging.basicConfig(filename=logFile,format='%(asctime)s %(message)s',filemode='w')
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+# Initialize configuration when starting up
+if config.value("theme") == "Auto":
+    qfluentwidgets.setTheme(qfluentwidgets.Theme.AUTO)
+elif config.value("theme") == "Light":
+    qfluentwidgets.setTheme(qfluentwidgets.Theme.LIGHT)
+elif config.value("theme") == "Dark":
+    qfluentwidgets.setTheme(qfluentwidgets.Theme.DARK)        
+else:
+    qfluentwidgets.setTheme(qfluentwidgets.Theme.AUTO)
+    config.setValue("theme", "Auto")
+
 class SettingsWindow (QMainWindow):
-       def __init__(self, parent):
-              super().__init__(parent)
+    def __init__(self, parent):
+        super().__init__(parent)
 
-              layout = QVBoxLayout()
-              central_widget = QWidget()
-              central_widget.setLayout(layout)
-              self.setCentralWidget(central_widget)
+        layout = QVBoxLayout()
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
-              
+        theme = ComboBox(items=["Auto","Light","Dark"], text='Theme')
+        theme.button.setCurrentText(config.value("theme"))
+        layout.addWidget(theme)
+        theme.button.currentTextChanged.connect(self.setTheme)  
+
+    def setTheme(self, theme:str):
+        config.setValue("theme", theme)
+        if theme == "Auto":
+            qfluentwidgets.setTheme(qfluentwidgets.Theme.AUTO)
+        elif theme == "Light":
+            qfluentwidgets.setTheme(qfluentwidgets.Theme.LIGHT)
+        elif theme == "Dark":
+            qfluentwidgets.setTheme(qfluentwidgets.Theme.DARK)            
