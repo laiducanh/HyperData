41ea09a2cf088b77c7027daf996232946af1a3a0
Lai <48932174@MacBook-Pro.local>
2024-08-19 22:19:35 +0700

    modify tooltip in plotview

--- HyperData/plot/plot_graphics_view.py
+++ HyperData/plot/plot_graphics_view.py
@@ -1,11 +1,14 @@
-from PyQt6.QtWidgets import (QGraphicsView, QGraphicsScene, QStyleOptionGraphicsItem, QWidget,
+from PyQt6.QtWidgets import (QGraphicsView, QGraphicsScene, QStyleOptionGraphicsItem, QGraphicsTextItem,
                              QWidget, QGraphicsItem, QGraphicsProxyWidget)
-from PyQt6.QtGui import QKeyEvent, QMouseEvent, QPainter, QPaintEvent
+from PyQt6.QtGui import QKeyEvent, QMouseEvent, QPainter, QPaintEvent, QPainterPath, QColor, QPen, QBrush, QTextOption
 from PyQt6.QtCore import QRectF, pyqtSignal, Qt
+import matplotlib.artist
+import matplotlib.axes
+import matplotlib.axis
 from plot.canvas import Canvas
 import matplotlib
 from ui.base_widgets.spinbox import _Slider
-from node_editor.node_node import Node
+from ui.utils import isDark
 
 class WidgetItem (QGraphicsItem):
     def __init__(self, widget, parent=None):
@@ -27,6 +30,65 @@ class WidgetItem (QGraphicsItem):
             self.width,
             self.height
         ).normalized()     
+    
+class ToolTip (QGraphicsItem):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.edge_size = 5.0
+        self.width = 180
+        self.height = 50
+
+        self.setColor()
+        self.setText()
+    
+    def setText (self, text=""):
+        for obj in self.childItems():
+            if isinstance(obj, QGraphicsTextItem):
+                obj.deleteLater()
+        self.title_item = QGraphicsTextItem(text, parent=self)
+        self.title_item.document().setDefaultTextOption(QTextOption(Qt.AlignmentFlag.AlignCenter))
+        self.title_item.setTextWidth(self.width)
+        
+    def setColor(self, color=Qt.GlobalColor.lightGray):
+                
+        self._pen_default = QPen(QColor(color))
+        self._pen_default.setWidthF(1.0)
+
+        if isDark():
+            self._brush_background = QBrush(QColor("#232323"))
+        else:
+            self._brush_background = QBrush(Qt.GlobalColor.white)
+
+    def boundingRect(self):
+        return QRectF(
+            0,
+            0,
+            self.width,
+            self.height
+        ).normalized()     
+
+    def paint(self, painter:QPainter, QStyleOptionGraphicsItem, widget=None):
+            
+        # content
+        path_content = QPainterPath()
+        path_content.setFillRule(Qt.FillRule.WindingFill)
+        path_content.addRoundedRect(0, 0, self.width, self.height, self.edge_size, self.edge_size)
+        path_content.addRect(0, 0, self.edge_size, self.edge_size)
+        path_content.addRect(self.width - self.edge_size, 0, self.edge_size, self.edge_size)
+        painter.setPen(Qt.PenStyle.NoPen)
+        painter.setBrush(self._brush_background)
+        painter.drawPath(path_content.simplified())
+
+
+        # outline
+        path_outline = QPainterPath()
+        path_outline.addRoundedRect(0, 0, self.width, self.height, self.edge_size, self.edge_size)
+        
+        painter.setBrush(Qt.BrushStyle.NoBrush)
+        painter.setPen(self._pen_default)
+        painter.drawPath(path_outline.simplified())
+
     
 class GraphicsView (QGraphicsView):
     sig_sidebar = pyqtSignal(str) # to call when sidebar needs to be updated
@@ -53,7 +115,7 @@ class GraphicsView (QGraphicsView):
         self.plotview = WidgetItem(canvas)
         self._scene.addItem(self.plotview) 
     
-        self.tooltip = Node(parent=parent)
+        self.tooltip = ToolTip()
         self._scene.addItem(self.tooltip)
         self.tooltip.hide()
 
@@ -81,6 +143,8 @@ class GraphicsView (QGraphicsView):
         for obj in stack:
             if obj._gid != None and obj.contains(event)[0]:
                 if not self.tooltip.isVisible():
+                    self.tooltip.setText(obj._gid.title())
+                    self.tooltip.setColor(obj._color)
                     self.tooltip.show()
                 break
             else: self.tooltip.hide()
