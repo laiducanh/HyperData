b3a419331f15205f4c390b9695d7108c41c5facf
Lai <48932174@MacBook-Pro.local>
2024-08-20 18:13:01 +0700

    fix bugs

--- HyperData/node_editor/node/data_cleaning.py
+++ HyperData/node_editor/node/data_cleaning.py
@@ -16,6 +16,7 @@ class NAEliminator (NodeContentWidget):
     def __init__(self, node: NodeGraphicsNode, parent=None):
         super().__init__(node, parent)
 
+        self.node.input_sockets[0].socket_data = pd.DataFrame()
         self._config = dict(axis='index',thresh='any',ignore_index=False)
     
     def initMenu(self):
@@ -58,21 +59,29 @@ class NAEliminator (NodeContentWidget):
             self.exec()
 
     def func(self):
+
         try:
-            if self._config["thresh"] in ["any","all"]:
+            if self._config["thresh"].isdigit():
                 self.node.output_sockets[0].socket_data = self.node.input_sockets[0].socket_data.dropna(axis=self._config["axis"],
-                                                          how=self._config["how"],
+                                                          thresh=int(self._config["thresh"]),
+                                                          ignore_index=self._config["ignore_index"])
+            
+            elif self._config["thresh"] in ["any","all"]:
+                self.node.output_sockets[0].socket_data = self.node.input_sockets[0].socket_data.dropna(axis=self._config["axis"],
+                                                          how=self._config["thresh"],
                                                           ignore_index=self._config["ignore_index"])
             else:
                 self.node.output_sockets[0].socket_data = self.node.input_sockets[0].socket_data.dropna(axis=self._config["axis"],
-                                                          thresh=int(self._config["how"]),
+                                                          how="any",
                                                           ignore_index=self._config["ignore_index"])
-            logger.info(f"{self.name} run successfully.")
+                
+            logger.info(f"{self.name} {self.node.id}::run successfully.")
         except Exception as e: 
-            self.node.output_sockets[0].socket_data = self.node.output_sockets[0].socket_data
-            logger.error(f"{self.name} {repr(e)}, return the original DataFrame.")
+            self.node.output_sockets[0].socket_data = self.node.input_sockets[0].socket_data
+            logger.error(f"{self.name} {self.node.id}::{repr(e)}, return the original DataFrame.")
         
         self.data_to_view = self.node.output_sockets[0].socket_data
+        
                 
     def eval(self):
         if self.node.input_sockets[0].edges == []:
@@ -83,7 +92,8 @@ class NAEliminator (NodeContentWidget):
 class NAImputer (NodeContentWidget):
     def __init__(self, node: NodeGraphicsNode, parent=None):
         super().__init__(node, parent)
-    
+
+        self.node.input_sockets[0].socket_data = pd.DataFrame()
         self._config = dict(imputer='univariate', m_strategy='mean', m_fill_value=None,
                             estimator=None, max_iter=10, tol=1e-3, u_strategy='mean', u_fill_value=None,
                             imputation_order='ascending', skip_complete=False,
@@ -101,21 +111,24 @@ class NAImputer (NodeContentWidget):
 
         univariate_widget = QWidget()
         univariate_layout = QVBoxLayout()
+        univariate_layout.setContentsMargins(0,0,0,0)
         univariate_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         univariate_widget.setLayout(univariate_layout)
         stacklayout.addWidget(univariate_widget)
         u_strategy = ComboBox(items=["next valid observation", "last valid observation", "mean","median","most_frequent","constant"],text='strategy')
-        u_strategy.button.currentTextChanged.connect(lambda s: u_fill_value.button.setEnabled(True) if s == 'Constant' 
-                                                   else u_fill_value.button.setEnabled(False))
-        u_strategy.button.setCurrentText(self._config['u_strategy'].title())
+        
+        u_strategy.button.setCurrentText(self._config['u_strategy'])
         univariate_layout.addWidget(u_strategy)
         u_fill_value = LineEdit(text='fill value')
         u_fill_value.button.setText(self._config['u_fill_value'])
-        u_fill_value.button.setEnabled(True if u_strategy.button.currentText() == 'Constant' else False)
+        u_fill_value.button.setEnabled(True if u_strategy.button.currentText() == 'constant' else False)
+        u_strategy.button.currentTextChanged.connect(lambda s: u_fill_value.button.setEnabled(True) if s == 'constant' 
+                                                   else u_fill_value.button.setEnabled(False))
         univariate_layout.addWidget(u_fill_value)
 
         multivariate_widget = QWidget()
         multivariate_layout = QVBoxLayout()
+        multivariate_layout.setContentsMargins(0,0,0,0)
         multivariate_widget.setLayout(multivariate_layout)
         stacklayout.addWidget(multivariate_widget)
 
@@ -131,7 +144,7 @@ class NAImputer (NodeContentWidget):
         m_strategy = ComboBox(items=["mean","median","most_frequent","constant"],text='strategy')
         m_strategy.button.currentTextChanged.connect(lambda s: m_fill_value.button.setEnabled(True) if s == 'Constant' 
                                                    else m_fill_value.button.setEnabled(False))
-        m_strategy.button.setCurrentText(self._config['m_strategy'].title())
+        m_strategy.button.setCurrentText(self._config['m_strategy'])
         multivariate_layout.addWidget(m_strategy)
         m_fill_value = LineEdit(text='fill value')
         m_fill_value.button.setText(self._config['m_fill_value'])
@@ -139,7 +152,7 @@ class NAImputer (NodeContentWidget):
         multivariate_layout.addWidget(m_fill_value)
 
         imputation_order = ComboBox(items=['ascending', 'descending', 'roman', 'arabic', 'random'], text='imputation order')
-        imputation_order.button.setCurrentText(self._config["imputation_order"].title())
+        imputation_order.button.setCurrentText(self._config["imputation_order"])
         multivariate_layout.addWidget(imputation_order)
 
         skip_complete = Toggle(text='Skip complete')
@@ -148,6 +161,7 @@ class NAImputer (NodeContentWidget):
 
         knn_widget = QWidget()
         knn_layout = QVBoxLayout()
+        knn_layout.setContentsMargins(0,0,0,0)
         knn_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         knn_widget.setLayout(knn_layout)
         stacklayout.addWidget(knn_widget)
@@ -157,22 +171,22 @@ class NAImputer (NodeContentWidget):
         knn_layout.addWidget(n_neighbors)
 
         weights = ComboBox(items=['uniform','distance'], text='weights')
-        weights.button.setCurrentText(self._config["weights"].title())
+        weights.button.setCurrentText(self._config["weights"])
         knn_layout.addWidget(weights)
 
         if dialog.exec(): 
-            self._config["imputer"] = imputer.button.currentText().lower()
-            self._config["u_strategy"] = u_strategy.button.currentText().lower()
+            self._config["imputer"] = imputer.button.currentText()
+            self._config["u_strategy"] = u_strategy.button.currentText()
             self._config["u_fill_value"] = u_fill_value.button.text()
-            self._config["m_strategy"] = m_strategy.button.currentText().lower()
+            self._config["m_strategy"] = m_strategy.button.currentText()
             self._config["m_fill_value"] = m_fill_value.button.text()
             self._config["estimator"] = None
             self._config["max_iter"] = max_iter.button.value()
             self._config["tol"] = tol.button.value()
-            self._config["imputation_order"] = imputation_order.button.currentText().lower()
+            self._config["imputation_order"] = imputation_order.button.currentText()
             self._config["skip_complete"] = skip_complete.button.isChecked()
             self._config["n_neighbors"] = n_neighbors.button.value()
-            self._config["weights"] = weights.button.currentText().lower()
+            self._config["weights"] = weights.button.currentText()
             self.exec()
 
     def func(self):
@@ -201,11 +215,11 @@ class NAImputer (NodeContentWidget):
                 self.node.output_sockets[0].socket_data = imputer.fit_transform(self.node.input_sockets[0].socket_data)
                 self.node.output_sockets[0].socket_data = pd.DataFrame(self.node.output_sockets[0].socket_data, columns=columns)            
             
-            logger.info(f"{self.name} run successfully.")
+            logger.info(f"{self.name} {self.node.id}::run successfully.")
 
         except Exception as e:
-            self.node.output_sockets[0].socket_data = self.node.output_sockets[0].socket_data
-            logger.error(f"{self.name} {repr(e)}, return the original DataFrame.")
+            self.node.output_sockets[0].socket_data = self.node.input_sockets[0].socket_data
+            logger.error(f"{self.name} {self.node.id}::{repr(e)}, return the original DataFrame.")
         
         self.data_to_view = self.node.output_sockets[0].socket_data        
 
@@ -219,13 +233,14 @@ class DropDuplicate (NodeContentWidget):
     def __init__(self, node: NodeGraphicsNode, parent=None):
         super().__init__(node, parent)
 
+        self.node.input_sockets[0].socket_data = pd.DataFrame()
         self._config = dict(keep='first',ignore_index=False)
     
     def config(self):
         dialog = Dialog("Configuration", self.parent.parent)
 
         keep = ComboBox(items=["first","last","none"], text='keep')
-        keep.button.setCurrentText(self._config["keep"].title())
+        keep.button.setCurrentText(self._config["keep"])
         dialog.main_layout.addWidget(keep)
 
         ignore_index = Toggle(text='ignore index')
@@ -233,19 +248,22 @@ class DropDuplicate (NodeContentWidget):
         dialog.main_layout.addWidget(ignore_index)
 
         if dialog.exec():
-            self._config["keep"] = keep.button.currentText().lower()
+            self._config["keep"] = keep.button.currentText()
             self._config["ignore_index"] = ignore_index.button.isChecked()
             self.exec()
     
-    def exec(self):
+    def func(self):
+
         if self._config["keep"] == 'none': keep = False
         else: keep = self._config["keep"]
+
         try:
-            self.node.output_sockets[0].socket_data = self.node.input_sockets[0].socket_data.drop_duplicates(keep=keep,ignore_index=self._config["ignore_index"])
-            logger.info(f"{self.name} run successfully.")
+            self.node.output_sockets[0].socket_data = self.node.input_sockets[0].socket_data.drop_duplicates(keep=keep,
+                                                                                                             ignore_index=self._config["ignore_index"])
+            logger.info(f"{self.name} {self.node.id}::run successfully.")
         except Exception as e:
-            self.node.output_sockets[0].socket_data = self.node.output_sockets[0].socket_data
-            logger.error(f"{self.name} {repr(e)}, return the original DataFrame.")
+            self.node.output_sockets[0].socket_data = self.node.input_sockets[0].socket_data
+            logger.error(f"{self.name} {self.node.id}::{repr(e)}, return the original DataFrame.")
         
         self.data_to_view = self.node.output_sockets[0].socket_data
 
