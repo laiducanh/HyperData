5702c3aec22bc6d70c339066d6e1f7be8d3982af
Lai <48932174@MacBook-Pro.local>
2024-08-22 21:27:02 +0700

    fig bugs and change LabelEncoder into LabelBinarizer

--- HyperData/node_editor/node/data_encoder.py
+++ HyperData/node_editor/node/data_encoder.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from node_editor.base.node_graphics_node import NodeGraphicsNode
 from sklearn.experimental import enable_iterative_imputer
-from sklearn.preprocessing import LabelEncoder as sk_LabelEncoder
+from sklearn.preprocessing import LabelBinarizer as sk_LabelEncoder
 from sklearn.preprocessing import OrdinalEncoder as sk_OrdinalEncoder
 from sklearn.preprocessing import OneHotEncoder as sk_OneHotEncoder
 
@@ -20,15 +20,19 @@ class LabelEncoder (NodeContentWidget):
         pass
 
     def func(self):
+        
         try:
-            encoder = sk_LabelEncoder()
+            encoder = sk_LabelEncoder() # LabelBinarizer
+            transform = encoder.fit_transform(self.node.input_sockets[0].socket_data)
+            self.node.output_sockets[0].socket_data = self.node.input_sockets[0].socket_data.copy()
             columns = self.node.input_sockets[0].socket_data.columns
-            self.node.output_sockets[0].socket_data = encoder.fit_transform(self.node.input_sockets[0].socket_data)
-            self.node.output_sockets[0].socket_data = pd.DataFrame(self.node.output_sockets[0].socket_data, columns=columns)
-            logger.info(f"{self.name} run successfully.")
+            for ind, val in enumerate(transform):
+                self.node.output_sockets[0].socket_data.loc[[ind], columns] = pd.Series([val], index=[ind])
+            
+            logger.info(f"{self.name} {self.node.id}::run successfully.")
         except Exception as e:
             self.node.output_sockets[0].socket_data = pd.DataFrame()
-            logger.error(f"{self.name} {repr(e)}, return an empty DataFrame.")
+            logger.error(f"{self.name} {self.node.id}::{repr(e)}, return an empty DataFrame.")
         
         self.data_to_view = self.node.output_sockets[0].socket_data        
     
@@ -51,10 +55,10 @@ class OrdinalEncoder (NodeContentWidget):
             columns = self.node.input_sockets[0].socket_data.columns
             self.node.output_sockets[0].socket_data = encoder.fit_transform(self.node.input_sockets[0].socket_data)
             self.node.output_sockets[0].socket_data = pd.DataFrame(self.node.output_sockets[0].socket_data, columns=columns)
-            logger.info(f"{self.name} run successfully.")
+            logger.info(f"{self.name} {self.node.id}::run successfully.")
         except Exception as e:
             self.node.output_sockets[0].socket_data = pd.DataFrame()
-            logger.error(f"{self.name} {repr(e)}, return an empty DataFrame.")
+            logger.error(f"{self.name} {self.node.id}::{repr(e)}, return an empty DataFrame.")
         
         self.data_to_view = self.node.output_sockets[0].socket_data        
     
@@ -71,13 +75,13 @@ class OneHotEncoder (NodeContentWidget):
     def config(self):
         pass
 
-    def exec(self):
+    def func(self):
         try:
             self.node.output_sockets[0].socket_data = pd.get_dummies(self.node.input_sockets[0].socket_data)
-            logger.info(f"{self.name} run successfully.")
+            logger.info(f"{self.name} {self.node.id}::run successfully.")
         except Exception as e:
             self.node.output_sockets[0].socket_data = pd.DataFrame()
-            logger.error(f"{self.name} {repr(e)}, return an empty DataFrame.")
+            logger.error(f"{self.name} {self.node.id}::{repr(e)}, return an empty DataFrame.")
 
         self.data_to_view = self.node.output_sockets[0].socket_data        
     
