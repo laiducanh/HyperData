edd6630b325dcef9bccdd7dca879e981baec9431
Duc Anh Lai <alai@smu.edu>
2024-12-06 13:20:40 -0600

    clean up

--- HyperData/plot/axes/axes.py
+++ HyperData/plot/axes/axes.py
@@ -9,12 +9,11 @@ from plot.canvas import Canvas
 class Spine (QWidget):
     def __init__(self, canvas:Canvas, parent=None):
         super().__init__(parent)
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-        self.layout.setContentsMargins(0,0,0,0)
+        self.vlayout = QVBoxLayout(self)
+        self.vlayout.setContentsMargins(0,0,0,0)
 
         self.spine2d = Spine2D(canvas, parent)
-        self.layout.addWidget(self.spine2d)
+        self.vlayout.addWidget(self.spine2d)
     
     def choose_axis_func(self, axis):
         self.spine2d.choose_axis._onClick(axis)
@@ -23,12 +22,11 @@ class Spine (QWidget):
 class Tick (QWidget):
     def __init__(self, canvas:Canvas, parent=None):
         super().__init__(parent)
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-        self.layout.setContentsMargins(0,0,0,0)
+        self.vlayout = QVBoxLayout(self)
+        self.vlayout.setContentsMargins(0,0,0,0)
 
         self.tick2d = Tick2D(canvas, parent)
-        self.layout.addWidget(self.tick2d)
+        self.vlayout.addWidget(self.tick2d)
 
     def choose_axis_func(self, axis):
         self.tick2d.choose_axis._onClick(axis)
