e8dc79ec1c5999abd0fd6913d85f894a128ccef9
Duc Anh Lai <alai@smu.edu>
2024-12-06 19:35:29 -0600

    clean up

--- HyperData/node_editor/node_node.py
+++ HyperData/node_editor/node_node.py
@@ -8,7 +8,6 @@ from node_editor.node.meta_classifier import MetaClassifier
 from node_editor.node.train_test_split import TrainTestSplitter
 from node_editor.node.figure import *
 from node_editor.node.misc import *
-from PyQt6.QtGui import QMouseEvent
 
 SINGLE_IN = 1
 MULTI_IN = 2
@@ -17,72 +16,76 @@ MULTI_OUT = 4
 
 class Node(NodeGraphicsNode):
     def __init__(self, title="Undefined Node",parent=None):
-        if title == 'Undefined Node':
-            super().__init__(title=title, inputs=[SINGLE_IN, MULTI_IN], outputs=[SINGLE_OUT, MULTI_OUT, SINGLE_OUT, SINGLE_OUT])
-            self.content = NodeContentWidget(self,parent)
-        elif title == 'Figure':
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[])
-            self.content = Figure(self,parent)
-        elif title == 'Data Reader':
-            super().__init__(title=title, inputs=[], outputs=[MULTI_OUT])
-            self.content = DataReader(self,parent)
-        elif title == 'Data Holder':
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = DataHolder(self,parent)
-        elif title == "Data Transpose":
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = DataTranspose(self, parent)
-        elif title == 'Data Concator':
-            super().__init__(title=title, inputs=[MULTI_IN], outputs=[MULTI_OUT])
-            self.content = DataConcator(self,parent)
-        elif title == 'Data Combiner':
-            super().__init__(title=title, inputs=[MULTI_IN], outputs=[MULTI_OUT])
-            self.content = DataCombiner(self,parent)
-        elif title == 'Data Merge':
-            super().__init__(title=title, inputs=[SINGLE_IN, SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = DataMerge(self,parent)
-        elif title == 'Data Compare':
-            super().__init__(title=title, inputs=[SINGLE_IN, SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = DataCompare(self,parent)
-        elif title == "Data Locator":
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = DataLocator(self,parent)
-        elif title == "Data Filter":
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = DataFilter(self,parent)
-        elif title == 'Nan Eliminator':
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = NAEliminator(self,parent)
-        elif title == 'Nan Imputer':
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = NAImputer(self,parent)
-        elif title == "Drop Duplicate":
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = DropDuplicate(self,parent)
-        elif title == "Label Encoder":
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = LabelEncoder(self,parent)
-        elif title == "Ordinal Encoder":
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = OrdinalEncoder(self,parent)
-        elif title == "One-Hot Encoder":
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
-            self.content = OneHotEncoder(self,parent)
-        elif title == "Train/Test Splitter":
-            super().__init__(title=title, inputs=[SINGLE_IN, SINGLE_IN], outputs=[MULTI_OUT, MULTI_OUT])
-            self.content = TrainTestSplitter(self,parent)
-        elif title == "Classifier":
-            super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT, MULTI_OUT])
-            self.content = Classifier(self,parent)
-        elif title == "Meta-Classifier":
-            super().__init__(title=title, inputs=[SINGLE_IN, MULTI_IN], outputs=[MULTI_OUT, MULTI_OUT])
-            self.content = MetaClassifier(self,parent)
-        elif title == 'Executor':
-            super().__init__(title=title, inputs=[], outputs=[])
-            self.content = Executor(self,parent)
-        elif title == 'User Define Card':
-            super().__init__(title=title, inputs=[], outputs=[])
-            self.content = UserDefine(self,parent)
+        match title:
+            case 'Undefined Node':
+                super().__init__(title=title, inputs=[SINGLE_IN, MULTI_IN], outputs=[SINGLE_OUT, MULTI_OUT, SINGLE_OUT, SINGLE_OUT])
+                self.content = NodeContentWidget(self,parent)
+            case 'Figure 2D':
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[])
+                self.content = Figure2D(self,parent)
+            case 'Figure 3D':
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[])
+                self.content = Figure3D(self,parent)
+            case 'Data Reader':
+                super().__init__(title=title, inputs=[], outputs=[MULTI_OUT])
+                self.content = DataReader(self,parent)
+            case 'Data Holder':
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = DataHolder(self,parent)
+            case "Data Transpose":
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = DataTranspose(self, parent)
+            case 'Data Concator':
+                super().__init__(title=title, inputs=[MULTI_IN], outputs=[MULTI_OUT])
+                self.content = DataConcator(self,parent)
+            case 'Data Combiner':
+                super().__init__(title=title, inputs=[MULTI_IN], outputs=[MULTI_OUT])
+                self.content = DataCombiner(self,parent)
+            case 'Data Merge':
+                super().__init__(title=title, inputs=[SINGLE_IN, SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = DataMerge(self,parent)
+            case 'Data Compare':
+                super().__init__(title=title, inputs=[SINGLE_IN, SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = DataCompare(self,parent)
+            case "Data Locator":
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = DataLocator(self,parent)
+            case "Data Filter":
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = DataFilter(self,parent)
+            case 'Nan Eliminator':
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = NAEliminator(self,parent)
+            case 'Nan Imputer':
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = NAImputer(self,parent)
+            case "Drop Duplicate":
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = DropDuplicate(self,parent)
+            case "Label Encoder":
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = LabelEncoder(self,parent)
+            case "Ordinal Encoder":
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = OrdinalEncoder(self,parent)
+            case "One-Hot Encoder":
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT])
+                self.content = OneHotEncoder(self,parent)
+            case "Train/Test Splitter":
+                super().__init__(title=title, inputs=[SINGLE_IN, SINGLE_IN], outputs=[MULTI_OUT, MULTI_OUT])
+                self.content = TrainTestSplitter(self,parent)
+            case "Classifier":
+                super().__init__(title=title, inputs=[SINGLE_IN], outputs=[MULTI_OUT, MULTI_OUT])
+                self.content = Classifier(self,parent)
+            case "Meta-Classifier":
+                super().__init__(title=title, inputs=[SINGLE_IN, MULTI_IN], outputs=[MULTI_OUT, MULTI_OUT])
+                self.content = MetaClassifier(self,parent)
+            case 'Executor':
+                super().__init__(title=title, inputs=[], outputs=[])
+                self.content = Executor(self,parent)
+            case 'User Define Card':
+                super().__init__(title=title, inputs=[], outputs=[])
+                self.content = UserDefine(self,parent)
         
         self.menu = self.content.menu
         self.set_Content(self.content)
