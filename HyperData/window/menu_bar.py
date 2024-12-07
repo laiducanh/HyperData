9a5cfd6a3ec86590fc83d88ffd1e26db9fefa008
Duc Anh Lai <alai@smu.edu>
2024-12-06 19:27:54 -0600

    move save and load function to main

--- HyperData/window/menu_bar.py
+++ HyperData/window/menu_bar.py
@@ -1,22 +1,21 @@
-from PyQt6.QtWidgets import QMenuBar, QMainWindow, QFileDialog
+from PyQt6.QtWidgets import QMenuBar, QMainWindow
 from PyQt6.QtGui import QAction
-from PyQt6.QtCore import pyqtSignal
 from config.settings_window import SettingsWindow
 from window.update import UpdateDialog
 
 
 class MenuBar(QMenuBar):
-    load = pyqtSignal()
-    save = pyqtSignal()
     def __init__(self, parent:QMainWindow):
         super().__init__(parent)
+
+        # parent is the Main
     
         fileMenu = self.addMenu('File')
         action = QAction('Load',self)
-        action.triggered.connect(self.loadFromFile)
+        action.triggered.connect(self.parent().loadFromFile)
         fileMenu.addAction(action)
         action = QAction('Save',self)
-        action.triggered.connect(self.saveToFile)
+        action.triggered.connect(self.parent().saveToFile)
         fileMenu.addAction(action)
         action = QAction('\0Settings',self)
         action.triggered.connect(self.setting_onClick)
@@ -39,22 +38,3 @@ class MenuBar(QMenuBar):
     
     def checkupdate_onClick(self):
         self.updateDialog.exec()
-
-    def saveToFile(self):
-        dialog = QFileDialog(self)
-        
-        if dialog.exec():
-            filename = dialog.selectedFiles()[0]
-            with open(filename, "w") as file:
-                file.write( json.dumps( self.parent().serialize(), indent=4 ) )
-            print("saving to", filename, "was successfull.")
-    
-    def loadFromFile(self):
-        dialog = QFileDialog(self)
-        
-        if dialog.exec():
-            filename = dialog.selectedFiles()[0]
-            with open(filename, "r") as file:
-                raw_data = file.read()
-                data = json.loads(raw_data)
-                self.parent().deserialize(data)
