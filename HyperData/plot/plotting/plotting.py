9e0d0eeca6af4b4c8a9ddd0e3b994b44012c5e87
LDA <laiducanh20.3@gmail.com>
2024-12-08 20:51:53 -0600

    rewrite set legend function

--- HyperData/plot/plotting/plotting.py
+++ HyperData/plot/plotting/plotting.py
@@ -2,42 +2,75 @@ from plot.plotting.base.line import *
 from plot.plotting.base.column import *
 from plot.plotting.base.scatter import *
 from plot.plotting.base.pie import *
-from matplotlib.axes import Axes
-from matplotlib.artist import Artist
-from typing import List, Union, Literal
+from plot.plotting.base.stats import *
+from config.settings import GLOBAL_DEBUG, logger
+from plot.utilis import find_mpl_object
 from plot.canvas import Canvas
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from matplotlib.artist import Artist
+from matplotlib.lines import Line2D
+from matplotlib.collections import Collection
+from matplotlib.patches import Rectangle, Wedge, PathPatch
+from matplotlib.legend import Legend
+from matplotlib.font_manager import FontProperties
+from typing import List, Union, Literal
 
+DEBUG = True
 
 def remove_artist (ax:Axes, gid:str) -> List[Artist]:
-    #print('remove artist')
-    old_artist = list()
-    for obj in ax.figure.findobj():
-        if obj._gid != None and gid in obj._gid:
-            old_artist.append(obj)
-            obj.remove()
-            #print("remove", obj, obj._gid)
+    artist_removed = list()
+    for artist in find_mpl_object(figure=ax.figure,match=[Artist],gid=gid):
+        artist.remove()
+        if GLOBAL_DEBUG or DEBUG:
+            pass
+    return artist_removed
 
-    return old_artist
+def update_props (from_obj: Artist, to_obj: Artist, 
+                  include=list(), exclude:Literal["default",None]="default") -> None:  
+    try:      
+        #print("update props")
+        print(to_obj, from_obj)
+        to_obj_props = to_obj.properties()
+        from_obj_props = from_obj.properties()
 
-def update_props (from_obj: Union[Line2D], to_obj: Union[Line2D], 
-                  include=list(), exclude:Literal["default",None]="default"):        
-    #print("update props")
-    if exclude == None:
-        exclude = from_obj.properties().keys()
-    else:
-        exclude = ["xdata","ydata","xydata","data","transform","paths","height","width","offsets",
-               "offset_transform","sizes","x","y","xy","gid"]
+        to_obj.update_from(from_obj)
 
-    for _prop in from_obj.properties().keys():
-        if _prop not in exclude or _prop in include:
-            try:
-                to_obj.update({_prop:from_obj.properties()[_prop]})
-                #print("update", _prop, "value", from_obj.properties()[_prop])
-            except Exception as e:
-                pass
-                #print('cannot update property', _prop, 'because of', repr(e))
+        # for step plots
+        if isinstance(to_obj, Line2D):
+            to_obj.set(drawstyle=to_obj_props.get("drawstyle"))
+        
+    except Exception as e:
+        logger.exception(e)
 
-def set_legend(ax:Axes):
+def get_legend(canvas: Canvas) -> Legend:
+    return canvas.axesleg.get_legend()
+
+def set_legend(canvas: Canvas, *args, **kwargs):
+    try:
+        _handles = canvas.axes.get_legend_handles_labels()[0]   + \
+                canvas.axesx2.get_legend_handles_labels()[0] + \
+                canvas.axesy2.get_legend_handles_labels()[0] + \
+                canvas.axespie.get_legend_handles_labels()[0]
+        _labels = canvas.axes.get_legend_handles_labels()[1]   + \
+                canvas.axesx2.get_legend_handles_labels()[1] + \
+                canvas.axesy2.get_legend_handles_labels()[1] + \
+                canvas.axespie.get_legend_handles_labels()[1]
+        old_title = None
+
+        if get_legend(canvas): 
+            old_title = get_legend(canvas).get_title()
+            get_legend(canvas).remove()
+        if _handles != []:
+            _legend = canvas.axesleg.legend(_handles, _labels, draggable=True, *args, *kwargs)
+            if old_title: 
+                _legend.set_title(old_title.get_text())
+                _legend.get_title().update_from(old_title)
+            canvas.draw()
+    except Exception as e:
+        logger.exception(e)
+
+def _set_legend(ax:Axes):
     _artist = list()
     _gid = list()
     _label = list()
@@ -63,75 +96,66 @@ def set_legend(ax:Axes):
         if _artist != []:
             ax_leg.legend(handles=_artist,draggable=True)
 
-def plotting(X, Y, Z, T, ax:Axes, gid:str=None, plot_type:str=None, update=True, **kwargs) -> List[str]:
+def rescale_plot(figure:Figure) -> None:
+    for _ax in figure.axes:
+        _ax.relim()
+        _ax.autoscale()    
+
+def plotting(X, Y, Z, T, ax:Axes, gid:str=None, plot_type:str=None, **kwargs) -> List[Artist]:
     
-    old_artist = remove_artist(ax, gid)
+    # get old artist that will be replaced
+    # but its properties will apply to the new ones
+    artist_old = find_mpl_object(ax.figure,
+                                 match=[Line2D,Collection,Rectangle,Wedge,PathPatch],
+                                 gid=gid)
+    
+    remove_artist(ax, gid)
     
     # rescale all axes while remove old artists and add new artists
-    for _ax in ax.figure.axes:
-        _ax.relim()
-        _ax.autoscale()
-        
-    gidlist = list()    
-
-    if plot_type == "treemap":
-        # treemap cannot update props from old artists
-        artist = treemap(X, ax, gid, **kwargs)
-
-    else:
-
-        if plot_type == "2d line":
-            artist = line2d(X, Y, ax, gid, **kwargs)
-        elif plot_type == "2d step":
-            artist = step2d(X, Y, ax, gid, **kwargs)
-        elif plot_type == "2d area":
-            artist = fill_between(X, Y, 0, ax, gid, **kwargs)
-        elif plot_type == "fill between":
-            artist = fill_between(X, Y, Z, ax, gid, **kwargs)
-        elif plot_type == "2d stacked area":
-            artist = stackedarea(X, Y, ax, gid, **kwargs)
-        elif plot_type == "2d 100% stacked area":
-            artist = stackedarea100(X, Y, ax, gid, **kwargs)
-        elif plot_type == "2d column":
-            artist = column2d(X, Y, ax, gid, **kwargs)
-        elif plot_type == "2d clustered column":
-            artist = clusteredcolumn2d(X, Y, ax, gid, **kwargs)
-        elif plot_type == "2d stacked column":
-            artist = stackedcolumn2d(X, Y, ax, gid, **kwargs)
-        elif plot_type == "2d 100% stacked column":
-            artist = stackedcolumn2d100(X, Y, ax, gid, **kwargs)
-        elif plot_type == "marimekko":
-            artist = marimekko(X, ax, gid, **kwargs)
-        elif plot_type == "2d scatter":
-            artist = scatter2d(X, Y, ax, gid, **kwargs)
-        elif plot_type == "bubble":
-            artist = bubble(X, Y, Z, ax, gid, **kwargs)
-        elif plot_type == "pie":
-            artist = pie(X, ax, gid, **kwargs)
-        elif plot_type == "doughnut":
-            artist = doughnut(X, ax, gid, **kwargs)
-        
-        #print("draw artist", artist)
-        
-        try:
-            for ind, obj in enumerate(artist):
-                update_props(old_artist[ind], obj, include=["label"], exclude=None)
-                if update:
-                    update_props(old_artist[ind],obj,exclude="default")
-        except: pass
+    rescale_plot(ax.figure)
     
+    match plot_type:
+        case "2d line":                 artist = line2d(X, Y, ax, gid, **kwargs)
+        case "2d step":                 artist = step2d(X, Y, ax, gid, **kwargs)
+        case "2d stem":                 artist = stem2d(X, Y, ax, gid, **kwargs)
+        case "2d area":                 artist = fill_between(X, Y, 0, ax, gid, **kwargs)
+        case "fill between":            artist = fill_between(X, Y, Z, ax, gid, **kwargs)
+        case "2d stacked area":         artist = stackedarea(X, Y, ax, gid, **kwargs)
+        case "2d 100% stacked area":    artist = stackedarea100(X, Y, ax, gid, **kwargs)
+        case "2d column":               artist = column2d(X, Y, ax, gid, **kwargs)
+        case "2d clustered column":     artist = clusteredcolumn2d(X, Y, ax, gid, **kwargs)
+        case "2d stacked column":       artist = stackedcolumn2d(X, Y, ax, gid, **kwargs)
+        case "2d 100% stacked column":  artist = stackedcolumn2d100(X, Y, ax, gid, **kwargs)
+        case "marimekko":               artist = marimekko(X, ax, gid, **kwargs)
+        case "treemap":                 artist = treemap(X, ax, gid, **kwargs)
+        case "2d scatter":              artist = scatter2d(X, Y, ax, gid, **kwargs)
+        case "2d bubble":               artist = bubble2d(X, Y, Z, ax, gid, **kwargs)
+        case "pie":                     artist = pie(X, ax, gid, **kwargs)
+        case "doughnut":                artist = doughnut(X, ax, gid, **kwargs)
+        case "histogram":               artist = histogram(X, ax, gid, **kwargs)
+        case "stacked histogram":       artist = stacked_histogram(X, ax, gid, **kwargs)
+        case "boxplot":                 artist = boxplot(X, ax, gid, **kwargs)
+        case "violinplot":              artist = violinplot(X, ax, gid, **kwargs)
+        case "eventplot":               artist = eventplot(X, ax, gid, **kwargs)
+        case "hist2d":                  artist = hist2d(X, Y, ax, gid, **kwargs)
+
+        case "3d line":                 artist = line3d(X, Y, Z, ax, gid, **kwargs)
+        case "3d step":                 artist = step3d(X, Y, Z, ax, gid, **kwargs)
+        case "3d stem":                 artist = stem3d(X, Y, Z, ax, gid, **kwargs)
+        case "3d column":               artist = column3d(X, Y, Z, ax, gid, **kwargs)
+        case "3d scatter":              artist = scatter3d(X, Y, Z, ax, gid, **kwargs)
+        case "3d bubble":               artist = bubble3d(X, Y, Z, T, ax, gid, **kwargs)
+    
+    # some plot types cannot update props from old artists
+    if plot_type not in ["treemap"]:
+        if artist_old != []:
+            for art in artist:
+                for art_old in artist_old:
+                    if art.get_gid() == art_old.get_gid():
+                        update_props(art_old, art)
     # update legend if necessary
-    set_legend(ax)
+    set_legend(ax.figure.canvas)
 
-    for ind, obj in enumerate(artist):
-
-        obj.plot_type = plot_type
-        gidlist.append(obj._gid)
-
-    # remove duplicates in gidlist
-    gidlist = list(set(gidlist))
-    #print("finish plotting", gidlist)
-    
     ax.figure.canvas.draw()
-
-    return gidlist
+    print("plotting")
+    return artist
