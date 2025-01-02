28cdefffdcac9285f8143825b51a62913b1da78b
LDA <laiducanh20.3@gmail.com>
2025-01-02 14:14:45 -0600

    fix menu signal

--- HyperData/plot/insert_plot/menu.py
+++ HyperData/plot/insert_plot/menu.py
@@ -27,61 +27,61 @@ class Menu_type_2D (Menu):
         bar.setIcon(icon_bar)
         for i in ['2d column', '2d clustered column', '2d stacked column', '2d 100% stacked column']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             bar.addAction(action)
         bar.addSeparator()
         for i in ['dot','clustered dot','stacked dot','dumbbell']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             bar.addAction(action)
         bar.addSeparator()
         for i in ['marimekko', 'treemap']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             bar.addAction(action)
 
         scatter = Menu('Scatter', self)
         scatter.setIcon(icon_scatter)
         for i in ['2d scatter']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             scatter.addAction(action)
        
         scatter.addSeparator()
         for i in ['2d bubble']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             scatter.addAction(action)
 
         pie = Menu('Pie', self)
         pie.setIcon(icon_pie)
         for i in ['pie','coxcomb']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             pie.addAction(action)
         pie.addSeparator()
         for i in ['doughnut','multilevel doughnut','semicircle doughnut']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             pie.addAction(action)
 
         stats = Menu('Statistics', self)
         stats.setIcon(icon_statistics)
         for i in ['histogram','stacked histogram','hist2d']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             stats.addAction(action)
         stats.addSeparator()
         for i in ['boxplot', 'violinplot','eventplot']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             stats.addAction(action)
 
         surface = Menu('Surface', self)
         surface.setIcon(icon_surface)
         for i in ['heatmap']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             surface.addAction(action)
 
         func = Menu('Function', self)
@@ -115,58 +115,58 @@ class Menu_type_3D (Menu):
         line.setIcon(icon_line)
         for i in ['3d line', '3d step','3d stem']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             line.addAction(action)
         
         bar = Menu('Column', self)
         bar.setIcon(icon_bar)
         for i in ['3d column']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             bar.addAction(action)
 
         scatter = Menu('Scatter', self)
         scatter.setIcon(icon_scatter)
         for i in ['3d scatter']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             scatter.addAction(action)
        
         scatter.addSeparator()
         for i in ['3d bubble']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             scatter.addAction(action)
 
         pie = Menu('Pie', self)
         pie.setIcon(icon_pie)
         for i in ['pie','doughnut']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             pie.addAction(action)
 
         stats = Menu('Statistics', self)
         stats.setIcon(icon_statistics)
         for i in ['histogram','stacked histogram']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             stats.addAction(action)
         stats.addSeparator()
         for i in ['boxplot', 'violinplot']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             stats.addAction(action)
 
         surface = Menu('Surface', self)
         surface.setIcon(icon_surface)
         for i in ['3d surface','triangular 3d surface']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             surface.addAction(action)
         surface.addSeparator()
         for i in ['heatmap']:
             action = Action(text=i.title(), parent=self)
-            action.triggered.connect(lambda type=i,checked=True: self.sig.emit(type.lower()))
+            action.triggered.connect(lambda _, type=i: self.sig.emit(type.lower()))
             surface.addAction(action)
 
         func = Menu('Function', self)
