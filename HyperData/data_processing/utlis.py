dd4d916ccc30049eb0f866d61d88872c46421dd0
LDA <laiducanh20.3@gmail.com>
2025-03-16 16:41:36 -0500

    change process input string

--- HyperData/data_processing/utlis.py
+++ HyperData/data_processing/utlis.py
@@ -1,5 +1,5 @@
-import sympy, pandas
-from config.settings import list_name
+import sympy, pandas, math, numpy, re
+from config.settings import list_name, logger
 
 def check_integer (input:str):
     try:
@@ -30,74 +30,51 @@ def evaluate_func (input:str,var):
                 except:pass
     return data_input
 
-def split_1d_data (input:str, data:pandas.DataFrame):
-
+def process_1d_data (input:str, data:pandas.DataFrame):
+    """ this function accepts input as a string of 1d data
+    in which element is separated by a comma
+    """
     data_input = []
-    
-    split1 = input.split(';')
+    split = input.split(',')
+
     try:
-        for i in split1:
-            try:
-                try:data_input.extend(eval(str(sympy.sympify(i))))
-                except:data_input.append(eval(str(sympy.sympify(i))))
+        for i in split:
+            try: 
+                _d = eval(i, {"numpy":numpy, "np": numpy, "math":math})
             except:
-                try:
-                    try:data_input.extend(eval('math.'+i))
-                    except:data_input.append(eval('math.'+i))
-                except:
-                    try:
-                        try:data_input.extend(eval('math.'+str(sympy.sympify(i))))
-                        except:data_input.append(eval('math.'+str(sympy.sympify(i))))
-                    except:
-                        try:
-                            try:data_input.extend(eval(i))
-                            except:data_input.append(eval(i))
-                        except:
-                            start_point = i.split(":")[0]
-                            end_point = i.split(":")[-1]
-                            start_point1 = (start_point.split(".")[0]).lower()
-                            start_point2 = start_point.split('.')[-1]
-                            end_point1 = (end_point.split(".")[0]).lower()
-                            end_point2 = end_point.split(".")[-1]
-                            if ":" in i:
-                                if start_point1 == end_point1:
-                                    if check_integer(start_point2) and check_integer(end_point2):
-                                        start_point2 = int(start_point2)
-                                        end_point2 = int(end_point2)
-                                    elif check_integer(start_point2): 
-                                        end_point2 = None
-                                        start_point2 = int(start_point2)
-                                    elif check_integer(end_point2):
-                                        start_point2 = 1
-                                        end_point2 = int(end_point2)
-                                    else:
-                                        start_point2 = 1
-                                        end_point2 = None
-                                    data_input.extend(data[data.columns[list_name.index(start_point1)]][start_point2-1:end_point2])
-                                    
-                                elif start_point2 == end_point2:
-                                    if start_point2.isdigit() and end_point2.isdigit():
-                                        data_input.extend(data.iloc[int(start_point2)-1][list_name.index(start_point1):list_name.index(end_point1)+1])
-                        
-                                
-                            elif check_float(i):
-                                data_input.append(float(i))
-                            elif "." in i:
-                                data_input.append(data[data.columns[list_name.index(start_point1)]][int(start_point2)-1])
-                            elif i != '':
-                                data_input.append(i)
-                                pass
-    except Exception as e: print(e)
+                # process input
+                start_point = i.split(":")[0]
+                end_point = i.split(":")[-1]
+                start_col = list_name.index(''.join(re.findall(r'[a-zA-Z]', start_point)).lower())
+                start_row = int(''.join(re.findall(r'\d', start_point))) - 1
+                end_col = list_name.index(''.join(re.findall(r'[a-zA-Z]', end_point)).lower()) + 1
+                end_row = int(''.join(re.findall(r'\d', end_point)))
+
+                _d = data.iloc[start_row : end_row, start_col : end_col].to_numpy()
+            
+            data_input.append(_d)
+            data_input = list(numpy.ravel(data_input))
+
+    except Exception as e: 
+        logger.exception("Cannot process input string.")
+        logger.exception(e)
+    
     return data_input
 
 def split_input(input:str, data:pandas.DataFrame):
+    """ this function will split a string input and
+    try to evaluate math expression from the string """
+  
     data_input = []
     input = input.replace(" ","")
+   
+    # if input is 2d data, which is separated by "|"
+    # split this input into a list of 1d data
     if "|" in input:
-        split1 = input.split("|")
-        for i in split1:
-            data_input.append(split_1d_data(i, data))
+        split = input.split("|")
+        for i in split:
+            data_input.append(process_1d_data(i, data))
     else:
-        data_input = split_1d_data(input, data)
-            
+        data_input = process_1d_data(input, data)
+    print(data_input)
     return data_input
