8d93a417881617ad61110d98181e9b1c31eda700
Duc Anh Lai <alai@smu.edu>
2025-06-29 20:12:25 -0500

    implement indeterminate progress bar

--- HyperData/ui/base_widgets/window.py
+++ HyperData/ui/base_widgets/window.py
@@ -1,12 +1,12 @@
 from PySide6.QtWidgets import (QProgressBar, QVBoxLayout, QProgressDialog, QDialog, 
-                               QHBoxLayout, QFileDialog, QWidget)
-from PySide6.QtCore import Signal, Qt, QPropertyAnimation, Property, QSize, QEasingCurve
-from PySide6.QtGui import QColor, QPainter, QPainterPath, QBrush, QPen
-import math, typing, platform
+                               QHBoxLayout, QFileDialog, QWidget,)
+from PySide6.QtCore import (Qt, QPropertyAnimation, Property, QEasingCurve
+                            ,QParallelAnimationGroup, QSequentialAnimationGroup)
+from PySide6.QtGui import QColor, QPainter, QPainterPath, QBrush
+import math, typing
 from ui.base_widgets.button import _PrimaryPushButton, _TransparentPushButton
-from ui.base_widgets.text import TitleLabel
-from ui.base_widgets.frame import SeparateHLine
 from ui.utils import isDark
+from typing import Literal
 
 class Dialog (QDialog):
     def __init__(self, title:str=None, parent=None):
@@ -77,23 +77,51 @@ class Dialog (QDialog):
 
     
 class ProgressBar(QProgressBar):
-    def __init__(self, parent=None):
+    def __init__(self, progresstype:Literal['normal','indeterminate']='indeterminate', 
+                 parent=None):
         super().__init__(parent)
         self.setFixedHeight(4)
-        self._val = 0
+        self.progresstype = progresstype
 
         self.lightBackgroundColor = QColor(0, 0, 0, 155)
         self.darkBackgroundColor = QColor(255, 255, 255, 155)
         self.color = QColor("#0085f1")
+
+        self._val = 0
         self.ani = QPropertyAnimation(self, b'val', self)
         self.setValue(self._val)
         self.valueChanged.connect(self._onValueChanged)
+
+        self._shortPos = 0
+        self._longPos = 0
+
+        self.shortBarAni = QPropertyAnimation(self, b'shortPos', self)
+        self.longBarAni = QPropertyAnimation(self, b'longPos', self)
+
+        self.aniGroup = QParallelAnimationGroup(self)
+        self.longBarAniGroup = QSequentialAnimationGroup(self)
+
+        self.shortBarAni.setDuration(833)
+        self.longBarAni.setDuration(1167)
+        self.shortBarAni.setStartValue(0)
+        self.longBarAni.setStartValue(0)
+        self.shortBarAni.setEndValue(1.45)
+        self.longBarAni.setEndValue(1.75)
+        self.longBarAni.setEasingCurve(QEasingCurve.Type.OutQuad)
+
+        self.aniGroup.addAnimation(self.shortBarAni)
+        self.longBarAniGroup.addPause(785)
+        self.longBarAniGroup.addAnimation(self.longBarAni)
+        self.aniGroup.addAnimation(self.longBarAniGroup)
+        self.aniGroup.setLoopCount(-1)
+
+        if progresstype == 'indeterminate': self.start()
     
-    def _setValue(self, value: int) -> None:
+    def set_value(self, value: int) -> None:
         self._val = value
         self.update()
     
-    def _value(self) -> int:
+    def get_value(self) -> int:
         return self._val
 
     def _onValueChanged(self, value):
@@ -108,6 +136,32 @@ class ProgressBar(QProgressBar):
             self.color = QColor("#e03131")
         elif colortype == "success":
             self.color = QColor("#0085f1")
+    
+    def get_shortPos(self):
+        return self._shortPos
+   
+    def set_shortPos(self, p):
+        self._shortPos = p
+        self.update()
+    
+    def get_longPos(self):
+        return self._longPos
+    
+    def set_longPos(self, p):
+        self._longPos = p
+        self.update()
+
+    def start(self):
+        self._shortPos = 0
+        self._longPos = 0
+        self.aniGroup.start()
+        self.update()
+
+    def stop(self):
+        self.aniGroup.stop()
+        self._shortPos = 0
+        self._longPos = 0
+        self.update()
 
     def paintEvent(self, e):
         painter = QPainter(self)
@@ -125,11 +179,29 @@ class ProgressBar(QProgressBar):
         # draw bar
         painter.setPen(Qt.PenStyle.NoPen)
         painter.setBrush(self.color)
-        w = int(self.val / (self.maximum() - self.minimum()) * self.width())
-        r = self.height() / 2
-        painter.drawRoundedRect(0, 0, w, self.height(), r, r)
 
-    val = Property(int, _value, _setValue)
+        # normal progress bar
+        if self.progresstype == 'normal':
+            w = int(self.val / (self.maximum() - self.minimum()) * self.width())
+            r = self.height() / 2
+            painter.drawRoundedRect(0, 0, w, self.height(), r, r)
+
+        else: # draw indeterminate progress bar
+            # draw short bar
+            x = int((self.shortPos - 0.4) * self.width())
+            w = int(0.4 * self.width())
+            r = self.height() / 2
+            painter.drawRoundedRect(x, 0, w, self.height(), r, r)
+
+            # draw long bar
+            x = int((self.longPos - 0.6) * self.width())
+            w = int(0.6 * self.width())
+            r = self.height() / 2
+            painter.drawRoundedRect(x, 0, w, self.height(), r, r)
+
+    val = Property(int, get_value, set_value)
+    shortPos = Property(float, get_shortPos, set_shortPos)
+    longPos = Property(float, get_longPos, set_longPos)
         
 class ProgressDialog (QProgressDialog):
     def __init__(self, text=None, cancel_btn=None, parent=None):
